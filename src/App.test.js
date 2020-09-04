a4125688338f12cb10787eb8a9dde8eb1d851774
cindyHua901 <455570742@qq.com>
2020-09-04 19:31:31 +0800

    add enzyme jest-enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+test("renders app", () => {
+  const wrapper = mount(<App />);
+  expect(wrapper.find("[data-test='container']")).toExist();
 });
